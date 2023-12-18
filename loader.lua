43c057ba05607d27d5e97bf9c9ffecbdec4919a4
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-12-18 23:51:05 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,37 @@
-
-
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
+local Window = OrionLib:MakeWindow({Name = "Blade Ball", HidePremium = false, IntroText = "Wreston", IntroEnabled = true, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Home = Window:MakeTab({
+	Name = "Test",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Home:AddSection({
+	Name = "Test2"
+})
+
+OrionLib:MakeNotification({
+	Name = "Title!",
+	Content = "Notification content... what will it say??",
+	Image = "rbxassetid://4483345998",
+	Time = 3
+})
+
+Home:AddButton({
+	Name = "Button!",
+	Callback = function()
+      		print("button pressed")
+  	end    
+})
+
+Home:AddToggle({
+	Name = "Toggle!",
+	Default = false,
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+
+CoolToggle:Set(false)
